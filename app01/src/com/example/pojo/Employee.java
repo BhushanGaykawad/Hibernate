d0a4f198f4a31e182c9f73095f8c6acb2a23a95d
bhushan <bhushan.gaikwad25@gmail.com>
2024-09-03 12:40:42 +0530

    Hibernate Applications

---
+++ app01/src/com/example/pojo/Employee.java
@@ -0,0 +1,36 @@
+package com.example.pojo;
+
+public class Employee {
+
+	private int eid;
+	private String ename;
+	private String eaddr;
+	private float esal;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEaddr() {
+		return eaddr;
+	}
+	public void setEaddr(String eaddr) {
+		this.eaddr = eaddr;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+	
+	
+	
+}
