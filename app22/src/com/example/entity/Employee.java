5383224d49750184acbea9a82f40c14ef7432bbe
bhushan <bhushan.gaikwad25@gmail.com>
2024-09-10 20:29:15 +0530

    Hibernate PK Annotation-- @GenericGenerator()

---
+++ app22/src/com/example/entity/Employee.java
@@ -0,0 +1,68 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="emp1")
+public class Employee {
+	@Id
+	@Column(name="eno")
+//	@TableGenerator(name="tableGenerator",table="my_table",pkColumnName="id",pkColumnValue="10", valueColumnName="next_hi")
+//	@GeneratedValue(strategy=GenerationType.TABLE,generator="tableGenerator")
+	@GenericGenerator(name="incGenerator",strategy="increment")
+	@GeneratedValue(generator="incGenerator")
+	private int eno;
+	
+//	private String eid;
+	@Column(name="ename")
+	private String ename;
+	
+	@Column(name="esal")
+	private float esal;
+	@Column(name="eaddr")
+	private String eaddr;
+	
+	public int getEno() {
+		return eno;
+	}
+	
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	
+//	public String getEid() {
+//		return eid;
+//	}
+//	public void setEid(String eid) {
+//		this.eid = eid;
+//	} 
+	
+	public String getEname() {
+		return ename;
+	}
+	
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+	public String getEaddr() {
+		return eaddr;
+	}
+	public void setEaddr(String eaddr) {
+		this.eaddr = eaddr;
+	}
+
+}
