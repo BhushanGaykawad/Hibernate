4caf69d2f44b93d768019edfdb344e8be1724588
bhushan <bhushan.gaikwad25@gmail.com>
2024-09-16 23:10:38 +0530

    Bulk Operations & HQL Query Parts

---
+++ app41/src/com/example/entity/Employee2.java
@@ -0,0 +1,47 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="emp2")
+public class Employee2 {
+
+	@Id
+	@Column(name="ENO")
+	private int eno;
+	@Column(name="ENAME")
+	private String ename;
+	@Column(name="EADDR")
+	private String eaddr;
+	@Column(name="ESAL")
+	private float esal;
+	
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEaddr() {
+		return eaddr;
+	}
+	public void setEaddr(String eaddr) {
+		this.eaddr = eaddr;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+
+}
